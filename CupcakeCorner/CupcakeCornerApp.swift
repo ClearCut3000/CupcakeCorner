b6c67aef55bf6dfb417d1fd41b20e1ee5ee6a16c
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-10-06 23:44:17 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Николай Никитин on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
